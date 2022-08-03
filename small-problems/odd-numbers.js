25ae44bbaba2bb70f76358f2340d3dfd92591ead
polinazhilyaeva <zhilyaeva.polina@gmail.com>
2022-08-03 23:06:04 +0100

    Add comments with the task description

--- small-problems/odd-numbers.js
+++ small-problems/odd-numbers.js
@@ -1,5 +1,11 @@
 /* Log all odd numbers from 1 to 99, inclusive, to the console,
  * with each number on a separate line.
+
+ * Further Exploration
+ * Repeat this exercise with a technique different from the one that you used,
+ * and different from the one provided.
+ * Also consider adding a way for the user to specify the limits
+ * of the odd numbers logged to the console.
  */
 
 const readLine = require('readline-sync');
